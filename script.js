444d99c1cdb5da409f5cd6f62dd070e626f73e12
Carina Meireles <74496779+cfcmeireles@users.noreply.github.com>
2023-02-05 00:21:16 +0000

    added event to remove active state on current target

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 (function (){
   "use strict";
 
-  const questions = document.querySelectorAll(".questions");
-  const answers = document.querySelectorAll(".answers");
+const accordions = document.querySelectorAll(".accordion");
   
-  for (let i = 0; i < questions.length; i++) {
-    questions[i].addEventListener("click", function(){
-      let height = answers[i].scrollHeight;
-      console.log(height);
-      answers[i].classList.toggle("active");
-      questions[i].classList.toggle("active");
-    });
+accordions.forEach(accordion => {
+ accordion.addEventListener("click", (event) => {
+  accordions.forEach(accordion => {
+    if (accordion !== event.currentTarget) {
+      accordion.classList.remove("active");
+    }
+  });
+    accordion.classList.toggle("active");
+ })
+})
   }
-})();
+)();
